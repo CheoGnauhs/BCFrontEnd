7d82bf728bc0fcf23197e8a9c22c8d8024169bfc
yansong <yansongsongsong@gmail.com>
2019-03-09 20:00:18 +0800

    add avatar username and buttons

--- src/components/OrderBody.vue
+++ src/components/OrderBody.vue
@@ -17,52 +17,43 @@ export default {
       orderData: [
         {
           picPath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
-          title: "商品名",
-          avatar: "卖家头像",
+          title: "商品名商品名商品名",
+          avatar: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
           description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述",
-          price: 100.12
+          price: 100.12,
+          userName: "asdeasd",
         },
         {
-          picPath: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2695614333,392862615&fm=26&gp=0.jpg",
+          picPath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
           title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
+          avatar: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
+          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述",
+          price: 100.12,
+          userName: "可爱的催化",
         },
         {
-          picPath: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2695614333,392862615&fm=26&gp=0.jpg",
+          picPath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
           title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
+          avatar: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
+          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述",
+          price: 100.12,
+          userName: "可爱的催化",
         },
         {
-          picPath: "",
+          picPath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
           title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
+          avatar: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
+          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述",
+          price: 100.12,
+          userName: "可爱的催化",
         },
         {
-          picPath: "",
+          picPath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
           title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
-        },
-        {
-          picPath: "",
-          title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
-        },
-        {
-          picPath: "",
-          title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
-        },
-        {
-          picPath: "",
-          title: "商品名",
-          avatar: "卖家头像",
-          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述"
+          avatar: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2254553630,4138028601&fm=26&gp=0.jpg",
+          description: "这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述这是一段商品描述",
+          price: 100.12,
+          userName: "可爱的催化",
         },
       ]
     };
--- src/components/OrderItem.vue
+++ src/components/OrderItem.vue
@@ -5,13 +5,29 @@
         <img v-bind:src="data.picPath" class="image">
         <div style="padding: 5px;">
           <el-row type="flex" class="row-bg" justify="space-between">
-            <span class="title">{{data.title.substr(0,8)}}</span>
+            <span class="title">{{data.title.length > 8? data.title.substr(0, 8)+'...': data.title}}</span>
             <span class="price"><font style="font-size: 14px">¥</font>{{data.price}}</span>
           </el-row>
           <el-row>
-            <el-col :span="24"><span class="descrip">{{data.description.substr(0, 20)}}</span></el-col>
+            <el-col :span="24"><span class="descrip">{{ data.description.length > 20? data.description.substr(0, 20)+'...': data.description }}</span></el-col>
           </el-row>
-          
+          <div class="divide-line"></div>
+          <el-row type="flex" class="row-bg" justify="space-between">
+            <el-col :span="4">
+              <div>
+                <img v-bind:src="data.avatar" class="avatar">
+                <span class="user-name">{{data.userName.length > 4? data.userName.substr(0, 3)+'...': data.userName}}</span>
+              </div>
+            </el-col>
+            <el-col :span="8" style="display: flex; align-items: center;">
+              <el-button type="danger" round size="medium" icon="el-icon-service">想要</el-button>
+            </el-col>
+            <el-col :span="8" style="display: flex; align-items: center;">
+              <el-button type="primary" round size="medium" icon="el-icon-goods">购买</el-button>
+            </el-col>
+          </el-row>
+          <el-row>
+          </el-row>
         </div>
       </el-card>
     </el-col>
@@ -19,6 +35,8 @@
 </template>
 
 <script>
+import '../assets/icon/iconheart.css'
+
 export default {
   name: "OrderItem",
   props: ['data'],
@@ -26,9 +44,25 @@ export default {
 </script>
 
 <style scoped>
+.user-name {
+  font-size: 12px
+}
+.icon {
+  padding: 0 5px 0 0;
+}
+.avatar {
+  border-radius: 10px;
+  width: 100%;
+}
+.divide-line {
+    height: 1px;
+    border-top: 1px solid #ddd;
+    text-align: center;
+    margin: 8px 8px 8px 8px
+}
 .price {
   font-size: 18px;
-  color: #fc0f3d;
+  color: #F56C6C;
   text-align: right;
 }
 .descrip {
